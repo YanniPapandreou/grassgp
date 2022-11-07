df500cec778051ee8ecea917d9567b99c7efac95
Yanni Papandreou <nepo1y@gmail.com>
2022-11-07 10:55:25 +0000

    script to analyse results

--- scripts/analyse.py
+++ scripts/analyse.py
@@ -19,18 +19,21 @@
 
 # %%
 import time
-from hydra_zen import instantiate, make_config, to_yaml, load_from_yaml
+from hydra_zen import to_yaml, load_from_yaml
 from pathlib import Path
 import os
 import jax.numpy as np
-from jax import random, vmap
+from jax import random 
 
 from grassgp.utils import load_and_convert_to_samples_dict as load_data
-from grassgp.plot_utils import flatten_samples, traceplots, plot_projected_data
-from grassgp.prediction import run_prediction_at_train_times
+from grassgp.plot_utils import flatten_samples, plot_projected_data, traceplots, plot_grids, plot_preds_train_locs, plot_grass_preds, plot_grass_dists, plot_preds, plot_AS_dir_preds, plot_fixed_x_preds_vs_time
+from grassgp.prediction import run_prediction_at_train_times, run_grass_predict, run_predict
+from grassgp.grassmann import compute_karcher_mean
 
-import matplotlib.pyplot as plt
-plt.rcParams["figure.figsize"] = (10,6)
+from train import sub_grid_inds
+
+import matplotlib.pyplot as ax
+ax.rcParams["figure.figsize"] = (10,6)
 
 
 # %%
@@ -43,8 +46,8 @@ def print_file(x: Path):
 # Job Parameters
 base_path = Path(os.getcwd())
 output_folder = "multirun"
-date = "2022-10-31"
-time = "17-36-39"
+date = "2022-11-02"
+time = "16-34-21"
 id_num = "0"
 
 # %%
@@ -55,8 +58,8 @@ assert job_path.exists()
 config = load_from_yaml(job_path / ".hydra" / "config.yaml")
 overrides = load_from_yaml(job_path / ".hydra" / "overrides.yaml")
 # print overrides and config
-print("Config used for training:")
-print(to_yaml(config))
+# print("Config used for training:")
+# print(to_yaml(config))
 print("Overrides used for training:")
 print(to_yaml(overrides))
 
@@ -67,8 +70,8 @@ dataset_path = Path(config.dataset_path) / "dataset.npz"
 
 dataset_config_path = Path(config.dataset_path) / ".hydra" / "config.yaml"
 assert dataset_config_path.exists()
-print("Config used to generate dataset:")
-print(to_yaml(load_from_yaml(dataset_config_path)))
+# print("Config used to generate dataset:")
+# print(to_yaml(load_from_yaml(dataset_config_path)))
 dataset_overrides_path = Path(config.dataset_path) / ".hydra" / "overrides.yaml"
 assert dataset_overrides_path.exists()
 print("Overrides used to generate dataset:")
@@ -108,144 +111,131 @@ assert set(samples.keys()).union(initial_values.keys()) == set(inference_data.ke
 # %%
 my_samples = flatten_samples(samples)
 
-# %% tags=[]
-# traceplots(my_samples, a=0.75)
+# %%
+print_file(job_path / "hmc_log.txt")
+
+# %% tags=[] jupyter={"source_hidden": true}
+# ignore_list = ['grass-L', 'grass-L_factor', 'grass-proj_mean', 'grass-proj_params', 'grass-sigmas', 'grass-standard_proj_params', 'grass-Ps']
+# for key, value in samples.items():
+#     if key in ignore_list:
+#         continue
+#     else:
+#         print(f"Current parameter is {key}")
+#         print("True value of parameter is:")
+#         if 'grass' in key:
+#             if 'kernel' in key:
+#                 cfg_param = config.inner_model.grass_config[key[len('grass-kernel_'):]]
+#             else:
+#                 cfg_param = config.inner_model.grass_config[key[len('grass-'):]]
+#             if cfg_param is None:
+#                 print(np.round(data[key],decimals=3))
+#             else:
+#                 print(np.round(np.array(cfg_param),decimals=3))
+#         elif 'reg' in key:
+#             cfg_param = config.outer_model.gp_config.params[key[len('reg-kernel_'):]]
+#             if cfg_param is None:
+#                 print(np.round(data[key], decimals=3))
+#             else:
+#                 print(np.round(np.array(cfg_param),decimals=3))
+        
+#         print("Mean value of parameter from HMC is:")
+#         print(np.round(value.mean(axis=0), decimals=3))
+#         print("Std. of parameter from HMC is:")
+#         print(np.round(value.std(axis=0), decimals=3))
 
 # %% tags=[]
-# plot_projected_data(X_fine_projs, s_fine, Ys_fine)
+traceplots(my_samples, a=1.0)
 
 # %% tags=[]
-# plot_projected_data(X_projs, s, Ys)
+plot_projected_data(X_fine_projs, s_fine, Ys_fine, cols=3, ex=0.95, fontsize=20)
 
 # %% tags=[]
-# plt.scatter(X_fine[:,0],X_fine[:,1],c='b',alpha=0.25,label='fine')
-# plt.scatter(X[:,0],X[:,1],c='r',label='coarse')
-# plt.legend()
-# plt.show()
+plot_projected_data(X_projs, s, Ys)
+
+# %% tags=[]
+plot_grids(X_fine, X)
 
 # %%
 pred_key = random.PRNGKey(3276359)
-means, predictions = run_prediction_at_train_times(pred_key, X, X, s, Ys, config, samples)
+means_train, predictions_train = run_prediction_at_train_times(pred_key, X, X, s, Ys, config, samples)
 
-# %%
-from grassgp.plot_utils import plot_preds_train_locs
-
-# %%
-plot_preds_train_locs(means, predictions, X, X, s, Ys, Ps, [2.5,97.5], fig_size=(12,8))
-
-# %%
-from grassgp.kernels import rbf
-from grassgp.grassmann import convert_to_projs
-from grassgp.utils import kron_solve, unvec
-import numpyro.distributions as dist
-
-
-# %%
-def grass_predict(key, s_test, s, anchor_point, Omega, var, length, noise, proj_params, reortho, jitter=5e-4):
-    D, n = anchor_point.shape
-    n_train = s.shape[0]
-    n_test = s_test.shape[0]
-    
-    # compute (temporal) kernels between train and test locs
-    grass_kernel_params = {'var': var, 'length': length, 'noise': noise}
-    T_K_pp = rbf(s_test, s_test, grass_kernel_params, include_noise=False)
-    T_K_pt = rbf(s_test, s, grass_kernel_params, include_noise=False)
-    T_K_tt = rbf(s, s, grass_kernel_params)
-    
-    # form M_covs between train and test locs
-    M_cov_pp = np.kron(T_K_pp, Omega)
-    M_cov_pt = np.kron(T_K_pt, Omega)
-    
-    # add jitter to M_cov_tt, M_cov_pp
-    M_cov_pp += jitter * np.eye(M_cov_pp.shape[0])
-    
-    # Get posterior cov for grass part    
-    T_K = M_cov_pp - np.matmul(M_cov_pt, vmap(lambda v: kron_solve(T_K_tt, Omega, v), in_axes=1, out_axes=1)(M_cov_pt.T))
-    
-    # Get posterior mean for grass part
-    T_mean = np.matmul(M_cov_pt, kron_solve(T_K_tt, Omega, proj_params))
-    
-    # sample projection params for test locs
-    T_sample = dist.MultivariateNormal(loc=T_mean, covariance_matrix=T_K).sample(key)
-    
-    
-    # split each up into params for each time
-    T_mean_split = np.array(T_mean.split(n_test))
-    T_sample_split = np.array(T_sample.split(n_test))
-    
-    # unvec each
-    unvec_T_mean = vmap(lambda params: unvec(params, D, n))(T_mean_split)
-    unvec_T_sample = vmap(lambda params: unvec(params, D, n))(T_sample_split)
-    
-    # form projector
-    I_UUT = (np.eye(D) - anchor_point @ anchor_point.T)
-    
-    # apply this to each
-    Deltas_mean = np.einsum('ij,ljk->lik', I_UUT, unvec_T_mean)
-    Deltas_sample = np.einsum('ij,ljk->lik', I_UUT, unvec_T_sample)
-    
-    # convert posterior means to projections
-    Ps_mean = convert_to_projs(Deltas_mean, anchor_point, reorthonormalize=reortho)
-    Ps_sample = convert_to_projs(Deltas_sample, anchor_point, reorthonormalize=reortho)
-    
-    # return Ps_mean and Ps_sample
-    return Ps_mean, Ps_sample
-
-
-# %%
-def run_grass_predict(pred_key, s_test, s, cfg, samples:dict, jitter=5e-4):
-    anchor_point = np.array(cfg.inner_model.grass_config.anchor_point)
-    n_samples = cfg.train.n_samples // cfg.train.n_thinning
-    proj_params_samples = samples['grass-proj_params']
-    assert n_samples == proj_params_samples.shape[0]
-    
-    # initialize vmap_args
-    vmap_args = (random.split(pred_key, n_samples), proj_params_samples)
-
-    cfg_Omega = cfg.inner_model.grass_config.Omega
-    cfg_var = cfg.inner_model.grass_config.var
-    cfg_length = cfg.inner_model.grass_config.length
-    cfg_noise = cfg.inner_model.grass_config.noise
-    require_noise = cfg.inner_model.grass_config.require_noise
-    reortho = cfg.inner_model.grass_config.reorthonormalize
-    
-    if cfg_Omega is None:
-        vmap_args += (samples['grass-Omega'],)
-    else:
-        cfg_Omega = np.array(cfg_Omega)
-        vmap_args += (np.repeat(cfg_Omega[None,:,:], n_samples, axis=0),)
-        
-    if cfg_var is None:
-        vmap_args += (samples['grass-kernel_var'],)
-    else:
-        vmap_args += (cfg_var * np.ones(n_samples),)
-        
-    if cfg_length is None:
-        vmap_args += (samples['grass-kernel_length'],)
-    else:
-        vmap_args += (cfg_length * np.ones(n_samples),)
-    
-    if require_noise:
-        if cfg_noise is None:
-            vmap_args += (samples['grass-kernel_noise'],)
-        else:
-            vmap_args += (cfg_noise * np.ones(n_samples),)
-    else:
-        vmap_args += (np.zeros(n_samples),)
-        
-    Ps_means, Ps_preds = vmap(lambda key, proj_params, Omega, var, length, noise: grass_predict(key, s_test, s, anchor_point, Omega, var, length, noise, proj_params, reortho, jitter=jitter))(*vmap_args)
-    return Ps_means, Ps_preds
+# %% tags=[]
+plot_preds_train_locs(means_train, predictions_train, X, X, s, Ys, Ps, [2.5,97.5], base_fig_size=(12,8))
 
 
 # %%
 pred_key = random.PRNGKey(457657)
 Ps_means, Ps_preds = run_grass_predict(pred_key, s_fine, s, config, samples)
 
-# %%
-from grassgp.plot_utils import plot_grass_preds, plot_grass_dists
+# %% tags=[]
 plot_grass_preds(s, s_fine, Ps_means, Ps_preds, Ps_fine, [2.5, 97.5])
 
-# %%
-plot_grass_dists(samples['grass-Ps'], Ps, s)
+# %% tags=[]
+plot_grass_dists(samples['grass-Ps'], Ps, s, base_fig_size=(6,4))
+
 
 # %%
+pred_key_test = random.PRNGKey(2352345)
+means_test, predictions_test, projs_test = run_predict(pred_key_test, X, s_fine, X, s, Ys, config, samples)
+
+
+# %%
+N_gap = config.subsample_conf.x_gap
+N_fine_sqrt = config.subsample_conf.n_x_samples
+inds = sub_grid_inds(N_gap,N_gap,N_fine_sqrt)
+assert (X_fine[inds,:] == X).all()
+
+# %% tags=[]
+plot_preds(means_test, predictions_test, X, X, s_fine, Ys_fine[inds,:], Ps_fine, s, [2.5, 97.5], base_fig_size=(12,8))
+
+# %%
+Ps_fixed = compute_karcher_mean(projs_test)
+
+# %% tags=[]
+plot_preds(means_test, predictions_test, X, X, s_fine, Ys_fine[inds,:], Ps_fixed, s, [2.5, 97.5], base_fig_size=(12,8))
+
+# %%
+plot_AS_dir_preds(projs_test, Ps_fine, s_fine, s)
+
+# %%
+plot_grass_dists(projs_test, Ps_fine, s_fine, base_fig_size=(6,4))
+
+# %%
+i = 10
+# i= 35
+x_fixed = X[i,:].reshape(1,-1)
+Ys_fixed = Ys[i,:].reshape(1,-1)
+print(x_fixed)
+print(Ys_fixed)
+
+pred_key_fixed_x= random.PRNGKey(423657658)
+n_s_grid = 40
+s_grid = np.linspace(0.,1.,n_s_grid)
+means_fixed_x, predictions_fixed_x, projs_fixed_x = run_predict(pred_key_fixed_x, x_fixed, s_grid, x_fixed, s, Ys_fixed, config, samples)
+plot_fixed_x_preds_vs_time(means_fixed_x, predictions_fixed_x, s_grid, s, x_fixed, Ys_fixed)
+
+# %%
+i= 35
+x_fixed = X[i,:].reshape(1,-1)
+Ys_fixed = Ys[i,:].reshape(1,-1)
+print(x_fixed)
+print(Ys_fixed)
+
+pred_key_fixed_x= random.PRNGKey(423657658)
+n_s_grid = 40
+s_grid = np.linspace(0.,1.,n_s_grid)
+means_fixed_x, predictions_fixed_x, projs_fixed_x = run_predict(pred_key_fixed_x, x_fixed, s_grid, x_fixed, s, Ys_fixed, config, samples)
+plot_fixed_x_preds_vs_time(means_fixed_x, predictions_fixed_x, s_grid, s, x_fixed, Ys_fixed)
+
+# %%
+i= 22
+x_fixed = X[i,:].reshape(1,-1)
+Ys_fixed = Ys[i,:].reshape(1,-1)
+print(x_fixed)
+print(Ys_fixed)
+
+pred_key_fixed_x= random.PRNGKey(423657658)
+n_s_grid = 40
+s_grid = np.linspace(0.,1.,n_s_grid)
+means_fixed_x, predictions_fixed_x, projs_fixed_x = run_predict(pred_key_fixed_x, x_fixed, s_grid, x_fixed, s, Ys_fixed, config, samples)
+plot_fixed_x_preds_vs_time(means_fixed_x, predictions_fixed_x, s_grid, s, x_fixed, Ys_fixed)
