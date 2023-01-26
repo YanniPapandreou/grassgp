8a8fc895928483ed4adce68bf299c0ac738a02a5
Yanni Papandreou <nepo1y@gmail.com>
2023-01-26 15:36:10 +0000

    updated prediction function for optimised matGP

--- src/grassgp/models_optimised.py
+++ src/grassgp/models_optimised.py
@@ -30,7 +30,7 @@ class MatGP:
         assert_shape(self.Omega_diag_chol, (d_n,),
                     custom_message=f"Omega_diag_chol has shape {self.Omega_diag_chol.shape}; expected shape {(d_n,)}")
 
-    def model(self, s: chex.ArrayDevice, use_kron_chol: bool = True) -> chex.ArrayDevice:
+    def model(self, s: chex.ArrayDevice) -> chex.ArrayDevice:
         d, n = self.d_out
         d_n = d * n
         assert_rank(s, self.d_in)
@@ -61,6 +61,58 @@ class MatGP:
         model = self.model
         seeded_model = handlers.seed(model, rng_seed=seed)
         return seeded_model(s)
+
+    def predict(self, key: chex.ArrayDevice, s_test: chex.ArrayDevice, s_train: chex.ArrayDevice, Vs_train: chex.ArrayDevice, jitter: float = 1e-8) -> Tuple[chex.ArrayDevice, chex.ArrayDevice]:
+        # TODO optimise this further to take advantage of diag structure of Omega
+        d, n = self.d_out
+        d_in = self.d_in
+        N_train = s_train.shape[0]
+        N_test = s_test.shape[0]
+        if d_in > 1:
+            assert s_train.shape[1] == d_in
+            assert s_test.shape[1] == d_in
+
+        # compute means
+        M_train = np.hstack(vmap(self.mu)(s_train))
+        M_test = np.hstack(vmap(self.mu)(s_test))
+        assert_shape(M_train, (d, n*N_train))
+        assert_shape(M_test, (d, n*N_test))
+
+        # compute kernels between train and test locs
+        K_train_train = self.k(s_train, s_train)
+        assert_shape(K_train_train, (N_train, N_train))
+        K_train_test = self.k(s_train, s_test)
+        assert_shape(K_train_test, (N_train, N_test))
+        K_test_train = K_train_test.T
+        K_test_test = self.k(s_test, s_test)
+        assert_shape(K_test_test, (N_test, N_test))
+
+        # compute posterior mean and cov
+        Omega_diag = self.Omega_diag_chol ** 2
+        Omega = np.diag(Omega_diag)
+        K_test_train_Omega = np.kron(K_test_train, Omega)
+        K_train_test_Omega = np.kron(K_train_test, Omega)
+        K_test_test_Omega = np.kron(K_test_test, Omega)
+        mean_sols = kron_solve(K_train_train, Omega, vec(np.hstack(Vs_train)) - vec(M_train))
+        vec_post_mean = vec(M_test) + K_test_train_Omega @ mean_sols
+        assert_shape(vec_post_mean, (d*n*N_test,),
+                     custom_message=f"vec_post_mean should have shape {(d*n*N_test,)}; obtained {vec_post_mean.shape}")
+
+        cov_sols = vmap(lambda v: kron_solve(K_train_train, Omega, v), in_axes=1, out_axes=1)(K_train_test_Omega)
+        post_cov = K_test_test_Omega - K_test_train_Omega @ cov_sols
+        assert_shape(post_cov, (d*n*N_test, d*n*N_test),
+                     custom_message=f"post_cov should have shape {(d*n*N_test,d*n*N_test)}; obtained {post_cov.shape}")
+
+        # sample predictions
+        post_cov += jitter * np.eye(d*n*N_test)
+        vec_pred = dist.MultivariateNormal(loc=vec_post_mean, covariance_matrix=post_cov).sample(key)
+        assert_shape(vec_pred, (d*n*N_test,),
+                     custom_message=f"vec_pred should have shape {(d*n*N_test,)}; obtained {vec_pred.shape}")
+
+        # unvec mean and preds and return
+        post_mean = vmap(lambda params: unvec(params, d, n))(np.array(vec_post_mean.split(N_test)))
+        pred = vmap(lambda params: unvec(params, d, n))(np.array(vec_pred.split(N_test)))
+        return post_mean, pred
 
 
 @chex.dataclass
