106d2ff8d3f1f3f83b7e5446ea4f10d43512d3bb
Yanni Papandreou <nepo1y@gmail.com>
2023-04-07 17:08:54 +0300

    added version in comments not working

--- shell.nix
+++ shell.nix
@@ -13,3 +13,15 @@ mkShell {
   shellHook = "fish";
 
 }
+
+# let
+# myAppEnv = poetry2nix.mkPoetryEnv {
+#   projectDir = ./.;
+#   editablePackageSources = {
+#     my-app = ./src;
+#   };
+#   overrides = poetry2nix.overrides.withDefaults(self: super: {
+#     pyarrow = super.pyarrow.override { preferWheel = true; };
+#   });
+# };
+# in myAppEnv.env
